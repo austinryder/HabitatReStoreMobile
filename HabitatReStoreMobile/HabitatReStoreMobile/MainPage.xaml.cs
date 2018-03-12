2cf73945f25d033dc453ee59a7f3da94046907d4
austinryder <rydera0988@forsythtech.edu>
2018-03-12 01:01:01 -0400

    Add project files.

---
+++ HabitatReStoreMobile/HabitatReStoreMobile/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HabitatReStoreMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
